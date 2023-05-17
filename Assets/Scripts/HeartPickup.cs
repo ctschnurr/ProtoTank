42685cf86d848ff7ef5dede7fb23e5b70e4914fa
ctschnurr <ctschnurr@hotmail.com>
2023-05-17 12:07:02 -0300

    made heart pickup 3d and now moves up/down and spins

--- Assets/Scripts/HeartPickup.cs
+++ Assets/Scripts/HeartPickup.cs
@@ -6,11 +6,19 @@ public class HeartPickup : Pickup
 {
 
     bool activated = false;
+    bool up = false;
+
+    Vector3 posChange;
+    float speed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerController>();
         screenManager = GameObject.Find("ScreenManager").GetComponent<ScreenManager>();
+
+        speed *= Time.deltaTime;
+        posChange = new Vector3(0, speed, 0);
     }
 
     // Update is called once per frame
@@ -21,6 +29,14 @@ public class HeartPickup : Pickup
             gameObject.SetActive(false);
             activated = false;
         }
+
+        if (transform.parent.localPosition.y > 1.25f) up = false;
+        if (transform.parent.localPosition.y < 1f) up = true;
+
+        if (up) transform.parent.localPosition += posChange;
+        if (!up) transform.parent.localPosition -= posChange;
+
+        transform.parent.Rotate(Vector3.up * 0.5f);
     }
 
     void OnTriggerEnter(Collider other)
